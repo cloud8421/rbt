1417c3a35aa1949ce235b9f925547e99235f5511
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-07-03 13:00:40 +0100

    Catch error when opening a channel

--- lib/rbt/channel.ex
+++ lib/rbt/channel.ex
@@ -2,9 +2,17 @@ defmodule Rbt.Channel do
   alias Rbt.Conn
 
   def open(conn_name) do
-    case Conn.get(conn_name) do
-      {:ok, conn} -> AMQP.Channel.open(conn)
-      error -> error
+    try do
+      case Conn.get(conn_name) do
+        {:ok, conn} -> AMQP.Channel.open(conn)
+        error -> error
+      end
+    catch
+      e ->
+        {:error, e}
+
+      _exit, e ->
+        {:error, e}
     end
   end
 end
