8cf4037ff7324ad15d918bb064c3ae9ad3e39c52
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-08-10 10:07:36 +0100

    Prepares 0.1.0 release
    Not usable, but good enough for packaging

--- mix.exs
+++ mix.exs
@@ -1,12 +1,23 @@
 defmodule Rbt.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description """
+  NOT COMPLETE OR PRODUCTION READY. A set of higher level tools to compose RabbitMQ workflows.
+  """
+
   def project do
     [
       app: :rbt,
+      version: @version,
+      description: @description,
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/cloud8421/rbt",
+      homepage_url: "https://github.com/cloud8421/rbt",
+      docs: [main: "readme", extras: ["README.md"]],
       deps: deps()
     ]
   end
@@ -28,6 +39,14 @@ defmodule Rbt.MixProject do
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Claudio Ortolina <cloud8421@gmail.com>"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/cloud8421/rbt"}
     ]
   end
 end
