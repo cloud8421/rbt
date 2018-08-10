a15e4b130717ccf4835c7fa8ab19b2d144c4d33d
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-08-10 10:01:17 +0100

    Add and setup ex_doc

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Rbt.MixProject do
     [
       {:ranch_proxy_protocol, "~> 2.0", override: true},
       {:amqp, "~> 1.0"},
-      {:jason, "~> 1.0", optional: true}
+      {:jason, "~> 1.0", optional: true},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
