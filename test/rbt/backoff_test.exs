78d9738d679968aa93fc506283fb6da1519d2e17
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-03 11:56:05 +0100

    Extract intervals generator
    This improves the error message, showing the interval
    that caused the test failure rather than the integer used
    to generate the interval itself.

--- test/rbt/backoff_test.exs
+++ test/rbt/backoff_test.exs
@@ -4,18 +4,22 @@ defmodule Rbt.BackoffTest do
 
   alias Rbt.Backoff
 
-  property "next interval is always between bounds" do
-    max_bound = Enum.count(Backoff.default_intervals())
-
-    forall n <- integer(1, max_bound) do
-      intervals = Enum.take(Backoff.default_intervals(), -n)
-
+  property "delay is always between bounds" do
+    forall intervals <- intervals(Backoff.default_intervals()) do
       state = %{backoff_intervals: intervals}
 
       {delay, new_state} = Backoff.next_interval(state)
 
       assert delay >= 100 and delay <= 30000
       assert Enum.count(new_state.backoff_intervals) >= 1
+    end
+  end
+
+  def intervals(default_intervals) do
+    max_bound = Enum.count(default_intervals)
+
+    let n <- integer(1, max_bound) do
+      Enum.take(default_intervals, -n)
     end
   end
 end
