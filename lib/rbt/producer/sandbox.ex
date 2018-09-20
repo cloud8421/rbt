58fea3498ddabb3d8d9b562a028f13751923a311
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-20 14:57:29 +0100

    Add specs and docs for Rbt.Producer.Sandbox

--- lib/rbt/producer/sandbox.ex
+++ lib/rbt/producer/sandbox.ex
@@ -15,6 +15,14 @@ defmodule Rbt.Producer.Sandbox do
   """
   alias Rbt.Producer.Event
 
+  @type exchange_name :: String.t()
+
+  @doc """
+  Creates the table necessary for the Sandbox to function.
+
+  This function can normally be added to `test/test_helper.exs`.
+  """
+  @spec create_table!() :: __MODULE__ | no_return
   def create_table! do
     opts = [
       :named_table,
@@ -24,6 +32,10 @@ defmodule Rbt.Producer.Sandbox do
     __MODULE__ = :ets.new(__MODULE__, opts)
   end
 
+  @doc """
+  Acts as a publisher, storing the generated event in memory.
+  """
+  @spec publish(exchange_name, Event.topic(), Event.data(), Event.opts()) :: :ok | no_return
   def publish(exchange_name, topic, event_data, opts) do
     publisher_pid = self()
     message_id = Keyword.get(opts, :message_id, Rbt.UUID.generate())
@@ -37,36 +49,60 @@ defmodule Rbt.Producer.Sandbox do
     store(exchange_name, event, publisher_pid)
   end
 
+  @doc """
+  Finds all events for the given exchange.
+  """
+  @spec find_by_exchange(exchange_name, pid()) :: [Event.t()]
   def find_by_exchange(exchange_name, producer_pid \\ self()) do
     spec = [{{:_, exchange_name, :_, producer_pid, :"$1"}, [], [:"$1"]}]
 
     :ets.select(__MODULE__, spec)
   end
 
+  @doc """
+  Counts all events for the given exchange.
+  """
+  @spec count_by_exchange(exchange_name, pid()) :: non_neg_integer()
   def count_by_exchange(exchange_name, producer_pid \\ self()) do
     spec = [{{:_, exchange_name, :_, producer_pid, :"$1"}, [], [true]}]
 
     :ets.select_count(__MODULE__, spec)
   end
 
+  @doc """
+  Finds all events for the given exchange and topic.
+  """
+  @spec find_by_exchange_and_topic(exchange_name, Event.topic(), pid()) :: [Event.t()]
   def find_by_exchange_and_topic(exchange_name, topic, producer_pid \\ self()) do
     spec = [{{:_, exchange_name, topic, producer_pid, :"$1"}, [], [:"$1"]}]
 
     :ets.select(__MODULE__, spec)
   end
 
+  @doc """
+  Counts all events for the given exchange and topic.
+  """
+  @spec count_by_exchange_and_topic(exchange_name, Event.topic(), pid()) :: non_neg_integer()
   def count_by_exchange_and_topic(exchange_name, topic, producer_pid \\ self()) do
     spec = [{{:_, exchange_name, topic, producer_pid, :"$1"}, [], [true]}]
 
     :ets.select_count(__MODULE__, spec)
   end
 
+  @doc """
+  Finds all events published by a given pid.
+  """
+  @spec find_by_producer_pid(pid()) :: [Event.t()]
   def find_by_producer_pid(producer_pid) do
     spec = [{{:_, :_, :_, producer_pid, :"$1"}, [], [:"$1"]}]
 
     :ets.select(__MODULE__, spec)
   end
 
+  @doc """
+  Counts all events published by a given pid.
+  """
+  @spec count_by_producer_pid(pid()) :: non_neg_integer()
   def count_by_producer_pid(producer_pid) do
     spec = [{{:_, :_, :_, producer_pid, :"$1"}, [], [true]}]
 
