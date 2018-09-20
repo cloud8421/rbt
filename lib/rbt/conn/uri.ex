ceefd162c29c37fc4d45c99112ac670e4ac5ef21
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-20 14:34:14 +0100

    Document Rbt.Conn.URI

--- lib/rbt/conn/uri.ex
+++ lib/rbt/conn/uri.ex
@@ -1,7 +1,24 @@
 defmodule Rbt.Conn.URI do
+  @moduledoc """
+  This module can be used to validate and manipulate AMQP URIs
+  as specified at <https://www.rabbitmq.com/uri-spec.html>.
+  """
+
   @type t :: String.t()
   @type merge_opts :: %{optional(binary()) => binary()}
 
+  @doc """
+  Validates a uri. Please refer to `amqp_uri.parse/1` (docs at
+  <https://hexdocs.pm/amqp_client/3.7.7/>) for details on the types of errors
+  that can be returned.
+
+      iex> Rbt.Conn.URI.validate("amqp://")
+      :ok
+      iex> Rbt.Conn.URI.validate("amqp:///test")
+      :ok
+      iex> Rbt.Conn.URI.validate("amqpp:///test")
+      {:error, {:unexpected_uri_scheme, 'amqpp'}}
+  """
   @spec validate(t) :: :ok | {:error, term()}
   def validate(uri) do
     case :amqp_uri.parse(uri) do
@@ -10,6 +27,16 @@ defmodule Rbt.Conn.URI do
     end
   end
 
+  @doc """
+  Allows merging an existing URI string with
+  options that needs overriding.
+
+  Specifically:
+
+      iex> uri = "amqp://guest:guest@localhost:15672/my-host?timeout=30"
+      iex> Rbt.Conn.URI.merge_options(uri, %{"timeout" => 60})
+      "amqp://guest:guest@localhost:15672/my-host?timeout=60"
+  """
   @spec merge_options(t, merge_opts) :: t
   def merge_options(base_uri, opts) do
     uri = URI.parse(base_uri)
---
+++ test/rbt/conn/uri_test.exs
@@ -0,0 +1,5 @@
+defmodule Rbt.Conn.URITest do
+  use ExUnit.Case, async: true
+
+  doctest Rbt.Conn.URI
+end
