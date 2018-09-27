67894d613151f82d3d7737b2699014c2388a205f
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-27 15:12:30 +0100

    Adds connection docs

--- lib/rbt/conn.ex
+++ lib/rbt/conn.ex
@@ -1,4 +1,12 @@
 defmodule Rbt.Conn do
+  @moduledoc """
+  This module implements a state machine that starts
+  and monitors a named connection which gets automatically
+  re-estabilished in case of issues.
+
+  Reconnection attempts implement a backoff logic.
+  """
+
   @behaviour :gen_statem
 
   alias Rbt.Conn.URI, as: ConnURI
@@ -23,6 +31,15 @@ defmodule Rbt.Conn do
   @type server_ref :: GenServer.server()
   @type start_ret :: {:ok, pid()} | {:error, term()}
 
+  @doc """
+  Implements a child specification suitable for use
+  as a worker in a supervision tree.
+
+      {Rbt.Conn, uri: "amqp://", name: :my_conn, open_opts: [heartbeat: 30_000]}
+
+  The last parameter, `open_opts`, defaults to a `[]` and gets passed directly to
+  `AMQP.Connection.open/1`.
+  """
   def child_spec(opts) do
     uri = Keyword.fetch!(opts, :uri)
     name = Keyword.fetch!(opts, :name)
@@ -37,20 +54,30 @@ defmodule Rbt.Conn do
     }
   end
 
+  @doc false
   @impl true
   def callback_mode, do: :state_functions
 
+  @doc """
+  Starts a connection given just a url: the connection is not named, uses default options.
+  """
   @spec start_link(url) :: start_ret()
   def start_link(url) do
     start_link(url, @default_open_opts)
   end
 
+  @doc """
+  Starts a connection given a url and open options: the connection is not named.
+  """
   @spec start_link(url, open_opts) :: start_ret()
   def start_link(uri, open_opts) do
     open_opts = Keyword.merge(@default_open_opts, open_opts)
     :gen_statem.start_link(__MODULE__, {uri, open_opts}, [])
   end
 
+  @doc """
+  Starts a connection given url, open options and name.
+  """
   @spec start_link(url, open_opts, name) :: start_ret()
   def start_link(uri, open_opts, name) when is_atom(name) do
     start_link(uri, open_opts, {:local, name})
@@ -61,6 +88,9 @@ defmodule Rbt.Conn do
     :gen_statem.start_link(name, __MODULE__, {uri, open_opts}, [])
   end
 
+  @doc """
+  Returns a `AMQP.Connection` struct given a connection name or pid.
+  """
   @spec get(server_ref()) ::
           {:ok, AMQP.Connection.t()} | {:error, :disconnected} | {:error, :non_existent}
   def get(ref) do
@@ -72,11 +102,15 @@ defmodule Rbt.Conn do
     end
   end
 
+  @doc """
+  Closes a connection given a connection name or pid.
+  """
   @spec close(server_ref()) :: :ok
   def close(ref) do
     :gen_statem.call(ref, :close)
   end
 
+  @doc false
   @impl true
   def init({uri, open_opts}) do
     case ConnURI.validate(uri) do
@@ -90,6 +124,7 @@ defmodule Rbt.Conn do
     end
   end
 
+  @doc false
   def disconnected(event_type, :try_connect, data)
       when event_type in [:internal, :state_timeout] do
     uri_with_options = ConnURI.merge_options(data.uri, data.open_opts)
@@ -114,6 +149,7 @@ defmodule Rbt.Conn do
     end
   end
 
+  @doc false
   def disconnected({:call, from}, :get, _data) do
     {:keep_state_and_data, {:reply, from, {:error, :disconnected}}}
   end
@@ -122,6 +158,7 @@ defmodule Rbt.Conn do
     {:stop_and_reply, :normal, {:reply, from, :ok}}
   end
 
+  @doc false
   def connected(:info, {:DOWN, ref, :process, pid, _reason}, data) do
     if data.mon_ref == ref and data.conn.pid == pid do
       {delay, new_data} = Backoff.next_interval(data)
