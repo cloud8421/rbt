9f898b51bc4cd379f51468f0213f41a6d1af22e0
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-28 16:11:48 +0100

    Improve docs for Rbt.Channel

--- lib/rbt/channel.ex
+++ lib/rbt/channel.ex
@@ -2,8 +2,13 @@ defmodule Rbt.Channel do
   @moduledoc """
   This module includes functions to interact with channels.
   """
+
   alias Rbt.Conn
 
+  @doc """
+  Given a named connection, try to open a channel on it.
+  """
+  @spec open(Conn.name()) :: {:ok, AMQP.Channel.t()} | {:error, term()}
   def open(conn_name) do
     case Conn.get(conn_name) do
       {:ok, conn} -> AMQP.Channel.open(conn)
@@ -11,6 +16,10 @@ defmodule Rbt.Channel do
     end
   end
 
+  @doc """
+  Closes a given channel.
+  """
+  @spec close(AMQP.Channel.t()) :: :ok | {:error, AMQP.Basic.error()}
   def close(channel) do
     AMQP.Channel.close(channel)
   end
