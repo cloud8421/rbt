25f9b809b97b9e77dea939f53778d13fcee9bc26
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-28 18:41:55 +0100

    Add docs for RPC instrumentation modules [ci skip]

--- lib/rbt/instrumentation/rpc/client.ex
+++ lib/rbt/instrumentation/rpc/client.ex
@@ -1,8 +1,29 @@
 defmodule Rbt.Instrumentation.Rpc.Client do
+  @moduledoc """
+  A module the implements this behaviour
+  can be used to instrument a `Rbt.Client` process.
+  """
+
+  @typedoc "A unique id for the rpc call."
   @type correlation_id :: String.t()
+
+  @typedoc """
+  The namespace used to identify the pool of servers capable of executing the rpc call.
+  """
   @type namespace :: String.t()
+
+  @typedoc """
+  The duration of the rpc call in microseconds.
+  """
   @type duration_in_us :: integer()
 
+  @doc """
+  Called right after the client successfully routed the rpc call.
+  """
   @callback on_rpc_start(namespace, correlation_id) :: :ok
+
+  @doc """
+  Called when the client receives a response for a pending rpc call.
+  """
   @callback on_rpc_end(namespace, correlation_id, duration_in_us) :: :ok
 end
--- lib/rbt/instrumentation/rpc/server.ex
+++ lib/rbt/instrumentation/rpc/server.ex
@@ -1,10 +1,42 @@
 defmodule Rbt.Instrumentation.Rpc.Server do
+  @moduledoc """
+  A module the implements this behaviour
+  can be used to instrument a `Rbt.Server` process.
+  """
+
+  @typedoc """
+  The namespace used to identify the pool of servers capable of executing the rpc call.
+  """
   @type namespace :: String.t()
+
+  @typedoc """
+  The duration of the rpc call in microseconds.
+  """
   @type duration_in_us :: integer()
+
+  @typedoc "The reason for the error"
   @type reason :: term()
 
+  @doc """
+  Called when the rpc server is booted. Can be used
+  to create a metric store if needed.
+  """
   @callback setup(namespace) :: :ok
+
+  @doc """
+  Called when the rpc server is stopped. Can be used
+  to perform any necessary cleanup.
+  """
   @callback teardown(namespace) :: :ok
+
+  @doc """
+  Called after the rpc server has executed an rpc call and published
+  its reply.
+  """
   @callback on_process(namespace, duration_in_us) :: :ok
+
+  @doc """
+  Called when the rpc server errors in executing the rpc call.
+  """
   @callback on_error(namespace, reason) :: :ok
 end
