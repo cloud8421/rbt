bb3a97b15f5a1bbb6da4bedd251f1b903a4a911c
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-09-28 12:25:48 +0100

    Use explicit ids for default task supervisors
    Fixes incompatibility with Elixir 1.6

--- lib/rbt/application.ex
+++ lib/rbt/application.ex
@@ -7,8 +7,12 @@ defmodule Rbt.Application do
     children = [
       Rbt.Registry.Consumer.child_spec(),
       Rbt.Registry.Producer.child_spec(),
-      {Task.Supervisor, name: Rbt.Consumer.DefaultTaskSupervisor},
-      {Task.Supervisor, name: Rbt.Rpc.DefaultTaskSupervisor}
+      Supervisor.child_spec({Task.Supervisor, name: Rbt.Consumer.DefaultTaskSupervisor},
+        id: Rbt.Consumer.DefaultTaskSupervisor
+      ),
+      Supervisor.child_spec({Task.Supervisor, name: Rbt.Rpc.DefaultTaskSupervisor},
+        id: Rbt.Rpc.DefaultTaskSupervisor
+      )
     ]
 
     opts = [strategy: :one_for_one, name: Rbt.Supervisor]
