87870c84deff276719af9d3214e1a07b56d71c37
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-04 15:49:22 +0100

    Add types and specs for Rbt.Consumer

--- lib/rbt/consumer.ex
+++ lib/rbt/consumer.ex
@@ -105,12 +105,51 @@ defmodule Rbt.Consumer do
             handler: nil,
             backoff_intervals: Backoff.default_intervals()
 
+  @type exchange_name :: String.t()
+  @type queue_name :: String.t()
+
+  @type config :: %{
+          max_workers: pos_integer(),
+          durable_objects: boolean(),
+          max_retries: pos_integer() | :infinity,
+          forward_failures: boolean(),
+          create_infrastructure: boolean(),
+          task_supervisor: module(),
+          instrumentation: module()
+        }
+
+  @type definitions :: %{
+          exchange_name: nil | exchange_name(),
+          queue_name: nil | queue_name(),
+          routing_keys: [String.t()]
+        }
+
+  @type t :: %__MODULE__{
+          conn_ref: nil | Rbt.Conn.name(),
+          channel: nil | AMQP.Channel.t(),
+          definitions: definitions(),
+          config: config(),
+          consumer_tag: nil | String.t(),
+          handler: nil | module(),
+          backoff_intervals: Backoff.intervals()
+        }
+
   ################################################################################
   ################################## PUBLIC API ##################################
   ################################################################################
 
+  @doc """
+  Start a consumer given a connection name and a handler module. Defaults to the
+  configuration provided by the handler module.
+  """
+  @spec start_link(Rbt.Conn.name(), module()) :: {:ok, pid()} | {:error, term()}
   def start_link(conn_ref, handler), do: start_link(conn_ref, handler, handler.config())
 
+  @doc """
+  Start a consumer given a connection name, a handler module and an explicit
+  configuration map.
+  """
+  @spec start_link(Rbt.Conn.name(), module(), config()) :: {:ok, pid()} | {:error, term()}
   def start_link(conn_ref, handler, config) do
     definitions = Map.fetch!(config, :definitions)
     exchange_name = Map.fetch!(definitions, :exchange_name)
@@ -124,26 +163,62 @@ defmodule Rbt.Consumer do
     )
   end
 
+  @doc """
+  Cancels consumption for a consumer (defined by exchange name and queue name).
+  """
+  @spec cancel(exchange_name(), queue_name()) ::
+          :ok | {:ok, :requested} | {:error, :in_progress} | {:error, :invalid}
   def cancel(exchange_name, queue_name) do
     :gen_statem.call(via(exchange_name, queue_name), :cancel)
   end
 
+  @doc """
+  Starts/resume consumption for a consumer (defined by exchange name and queue name).
+  """
+  @spec consume(exchange_name(), queue_name()) ::
+          :ok | {:ok, :requested} | {:error, :in_progress} | {:error, :invalid}
   def consume(exchange_name, queue_name) do
     :gen_statem.call(via(exchange_name, queue_name), :consume)
   end
 
+  @doc """
+  Changes capacity for a consumer (defined by exchange name and queue name).
+  This will both update the `prefetch_count` on the consumer channel.
+  """
+  @spec scale(exchange_name(), queue_name(), pos_integer()) :: :ok | {:error, :invalid}
   def scale(exchange_name, queue_name, max_workers) do
     :gen_statem.call(via(exchange_name, queue_name), {:scale, max_workers})
   end
 
+  @doc """
+  Returns the status for a consumer (defined by exchange name and queue name).
+  """
+  @spec status(exchange_name(), queue_name()) :: %{state: atom(), data: t()}
   def status(exchange_name, queue_name) do
     :gen_statem.call(via(exchange_name, queue_name), :status)
   end
 
+  @doc """
+  Returns the status for a consumer (defined by its pid).
+  """
+  @spec status(GenServer.name()) :: %{state: atom(), data: t()}
   def status(consumer_ref) do
     :gen_statem.call(consumer_ref, :status)
   end
 
+  @doc """
+  Returns the topology information for a consumer (defined by its pid).
+  """
+  @spec topology_info(GenServer.name()) :: %{
+          state: atom(),
+          conn_ref: Rbt.Conn.name(),
+          infrastructure: definitions(),
+          config: %{
+            forward_failures: boolean(),
+            max_retries: pos_integer() | :infinity,
+            max_workers: pos_integer()
+          }
+        }
   def topology_info(consumer_ref) do
     status = status(consumer_ref)
     config = Map.take(status.data.config, [:forward_failures, :max_retries, :max_workers])
@@ -160,6 +235,10 @@ defmodule Rbt.Consumer do
   ################################## CALLBACKS ###################################
   ################################################################################
 
+  @doc """
+  Returns a child specification suitable for inserting a consumer
+  inside a supervision tree.
+  """
   def child_spec(opts) do
     conn_ref = Keyword.fetch!(opts, :conn_ref)
     handler = Keyword.fetch!(opts, :handler)
@@ -174,8 +253,10 @@ defmodule Rbt.Consumer do
     }
   end
 
+  @doc false
   def callback_mode, do: :handle_event_function
 
+  @doc false
   def init({conn_ref, handler, opts}) do
     definitions = Map.fetch!(opts, :definitions)
     config = Map.take(opts, @config_keys)
@@ -201,6 +282,7 @@ defmodule Rbt.Consumer do
 
   # SETUP OBJECTS AND AUTO SUBSCRIPTION
 
+  @doc false
   def handle_event(event_type, :try_declare, :idle, data)
       when event_type in [:internal, :state_timeout] do
     case Channel.open(data.conn_ref) do
@@ -364,6 +446,7 @@ defmodule Rbt.Consumer do
     {:stop, reason}
   end
 
+  @doc false
   def terminate(_reason, _state, data) do
     instrument_teardown!(data)
 
